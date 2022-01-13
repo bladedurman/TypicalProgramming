ddf549123ed0dd63771b21273c8cfcff1e263b19
bladedurman <68160833+bladedurman@users.noreply.github.com>
2022-01-12 20:50:50 -0800

    Exit command

--- shell.py
+++ shell.py
@@ -1,7 +1,9 @@
 import basic
 
-while True:
+keep_going = True
+while keep_going:
     text = input('Typical ::> ')
+    if "quit" in text.lower() or "exit" in text.lower(): keep_going = False
     result, error = basic.run('<stdin>', text)
 
     if error: print(error.as_string())
