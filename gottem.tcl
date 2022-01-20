273ff717dec91ed7b3f56808c1e746580bea03bf
bladedurman <68160833+bladedurman@users.noreply.github.com>
2022-01-19 21:31:35 -0800

    added first program

---
+++ gottem.tcl
@@ -0,0 +1,11 @@
+while true do
+
+    print("Enter in any string below.");
+    var userInput = input();
+    print("Gottem");
+
+    print("Enter an Integer below vvvvvvvvv");
+    var userInt = inputInt();
+
+    if userInt == 6 then break
+    end
